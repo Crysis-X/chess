ce64ec7d2fa07a4a15fd5b01575297f6cde71f70
Crysis <spidrunner2008@gmail.com>
2024-09-15 09:31:39 +0300

    updated module Chess

--- src/modules/Chess/Chess.ts
+++ src/modules/Chess/Chess.ts
@@ -1,4 +1,6 @@
 import Cell from "../Cell/Cell";
+import MoveContext from "../MoveContext/MoveContext";
+import Pawn from "../Pawn/Pawn";
 
 type gameType = "online" | "double" | "offline";
 const vars = {
@@ -9,14 +11,24 @@ export default class Chess {
   private type: gameType;
   private board = document.createElement("div");
   private playerColor: "white" | "black";
-  private cells: Array<Cell | HTMLDivElement>[] = [];
+  private cells: Cell[][] = [];
+  private moveContext?: MoveContext;
   constructor(type: gameType, playerColor: "white" | "black" = "white") {
     this.type = type;
     this.board.className = vars.boardClasses;
     this.playerColor = playerColor;
-    console.log("chess");
   }
-  appendBoardInto = (element: HTMLElement) => element.append(this.board);
+  addInteract = () => {
+    if(!this.cells.length) return false;
+    this.moveContext = new MoveContext(this.cells);
+    this.moveContext.startListening();
+    return true;
+  }
+  appendBoardInto = (element: HTMLElement) => {
+    if(!this.cells.length) return false;
+    element.append(this.board);
+    return true;
+  }
   build = async () => {
     const chars = ["a", "b", "c", "d", "e", "f", "g", "h"];
     const nums = [1, 2, 3, 4, 5, 6, 7, 8].reverse();
@@ -24,32 +36,42 @@ export default class Chess {
       chars.reverse();
       nums.reverse();
     }
-    let isWhite = true;
+    let isWhite = false;
     for (let y = -1; y < 9; y++) {
-      const line: Array<HTMLDivElement | Cell> = [];
+      const line: Cell[] = [];
       for (let x = -1; x < 9; x++) {
-        let cell;
         if (y == -1 || y == 8) {
-          cell = document.createElement("div");
-          cell.className = vars.cellClasses + " bg-background";
-          if (x == -1 || x == 8) cell.innerHTML = "";
-          else cell.innerHTML = chars[x];
-        } else if (x == -1 || x == 8) {
-          cell = document.createElement("div");
-          cell.className = vars.cellClasses + " bg-background";
-          cell.innerHTML = nums[y].toString();
-        } else
-          cell = new Cell(
-            x,
-            y,
-            vars.cellClasses + " " + (isWhite ? "bg-white" : "bg-black"),
-          );
+          const voidCell = document.createElement("div");
+          voidCell.className = vars.cellClasses + " bg-background";
+          if (x == -1 || x == 8) voidCell.innerHTML = "";
+          else voidCell.innerHTML = chars[x];
+          this.board.append(voidCell);
+          continue;
+        }
+        if (x == -1 || x == 8) {
+          const voidCell = document.createElement("div");
+          voidCell.className = vars.cellClasses + " bg-background";
+          voidCell.innerHTML = nums[y].toString();
+          this.board.append(voidCell);
+          continue;
+        }
+        const cell = new Cell(x, y, "cell-" + x + "-" + y, 
+          vars.cellClasses + " " + (isWhite ? "bg-white" : "bg-black"));
         line.push(cell);
-        "getX" in cell ? cell.appendInto(this.board) : this.board.append(cell);
+        cell.appendInto(this.board);
         isWhite = !isWhite;
       }
-      this.cells.push(line);
+      if(line.length) this.cells.push(line);
       isWhite = !isWhite;
     }
+    this.spawnFigures();
   };
+  private spawnFigures = () => {
+    if(!this.cells.length) return false;
+    for(let i = 0; i < 8; i++){
+      this.cells[1][i].setFigure(new Pawn("black", this.playerColor == "white" ? "+" : "-"));
+      this.cells[6][i].setFigure(new Pawn("white", this.playerColor == "white" ? "-" : "+"));
+    }
+    return true;
+  }
 }
