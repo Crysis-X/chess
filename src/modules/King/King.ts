72c7c799ef50deed6294b9527d7418edbcbf5d34
Crysis <spidrunner2008@gmail.com>
2024-09-15 15:25:34 +0300

    created module King

---
+++ src/modules/King/King.ts
@@ -0,0 +1,38 @@
+import Figure from "../Figure/Figure";
+import king from "@/assets/king.png";
+import kingWhite from "@/assets/king-white.png";
+import Cell from "../Cell/Cell";
+
+export default class King extends Figure {
+  constructor(color: "white" | "black") {
+    super(color, color == "white" ? kingWhite.src : king.src);
+  }
+  canMoveTo = (cells: Cell[][], current: Cell, to: Cell) => {
+    const variants: Cell[] = [];
+    const plus = [
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+      { x: -1, y: 1 },
+      { x: 1, y: 0 },
+      { x: -1, y: 0 },
+      { x: 0, y: -1 },
+      { x: 1, y: -1 },
+      { x: -1, y: -1 },
+    ];
+    const y = current.getY();
+    const x = current.getX();
+    plus.forEach((data) => {
+      const cellY = y + data.y;
+      const cellX = x + data.x;
+      if (cells[cellY] && cells[cellY][cellX]) {
+        if (cells[cellY][cellX]?.getFigure()?.getColor() != this.getColor())
+          variants.push(cells[cellY][cellX]);
+      }
+    });
+    for (let i = 0; i < variants.length; i++) {
+      if (variants[i].getX() == to.getX() && variants[i].getY() == to.getY())
+        return true;
+    }
+    return false;
+  };
+}
